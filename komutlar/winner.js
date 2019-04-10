e98377810dc84c197a9da2ff12ea9f1ce6e7afb4
LordAlone <49336256+LordAlone@users.noreply.github.com>
2019-04-10 21:38:44 +0300

    Create winner.js

---
+++ komutlar/winner.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+var Jimp = require('jimp');
+
+exports.run = async (client, message, args) => {
+    var user = message.mentions.users.first() || message.author;
+    if (!message.guild) user = message.author;
+   
+    message.channel.send(`:timer:  // Lütfen Bekleyin`).then(m => m.delete(1000));
+
+Jimp.read(user.avatarURL, (err, image) => {
+    image.resize(315, 310)
+    Jimp.read("https://vignette.wikia.nocookie.net/totaldramaisland/images/5/5f/Winner.png/revision/latest?cb=20131104030406",(err, avatar) => {
+        avatar.resize(315, 320)
+        image.composite(avatar, 0, 0).write(`./img/snip/${client.user.id}-${user.id}.png`);
+        setTimeout(function() {
+            message.channel.send(new Discord.Attachment(`./img/snip/${client.user.id}-${user.id}.png`));
+        }, 1000);
+    });
+
+});
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'winner',
+    description: 'Winner efekti',
+    usage: 'winner'
+  };
